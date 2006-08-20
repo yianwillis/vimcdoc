12bbf6f7f47fb02db4ed5ef84b924e91c87274d3
wandys <wandys@856b2ddb-7513-0410-ad46-fe972a8d2c23>
2006-08-20 04:09:08 +0000

    1. use svn info instead of svnversion to get the numeric revision of last    tagged release (FYI, svnversion gets the revision of the working copy).
    2. detect newly added files since.
    
    3. do not output info if the file has no change.
    
    4. make the layout look similar to the old one.

--- tools/svnchanges.sh
+++ tools/svnchanges.sh
@@ -1,10 +1,31 @@
 #!/bin/bash
-version=VIMCDOC_`cat ../VERSION | sed -e 's/\./_/g'`
-last_rev=`svnversion ../../tags/$version`
 
+version=VIMCDOC_`cat ../VERSION | sed -e 's/\./_/g'`
+last_rev=`svn info ../../tags/$version | grep 'Last Changed Rev:' | cut -d ' ' -f 4`
+
+# decect added new files since last tagged release
+list=/tmp/vimcdoc-list.$$
+added=/tmp/vimcdoc-added.$$
 cd ../doc
-for i in *.txt;
+ls > $list
+ls ../../tags/$version/doc | comm -3 - $list > $added
+if [ -s $added ]; then
+	echo '>>>> NEW FILES SINCE LAST RELEASE <<<<'
+	cat $added
+	echo -e '\n'
+fi
+rm $list $added
+
+# output updating info of various files
+change=/tmp/vimcdoc-change.$$
+echo '>>>> UPDATED FILES SINCE LAST RELEASE <<<'
+for i in *.txt
 do
-    echo ~~~~ $i ~~~~
-    svn log -r $last_rev:HEAD  $i | sed -e '/^-----/d' -e '/^r[0-9]\+/d'
+    svn log -r $last_rev:HEAD  $i > $change
+    if [ -s $change ]; then
+	    echo -e "~~~~\t$i\t~~~~"
+	    cat $change
+	    echo -e '\n'
+    fi
 done
+rm $change
