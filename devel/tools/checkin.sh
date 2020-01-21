42af86a68946a80829de94ea53cfde7422473c4b
Yian Huang <yianwillis@gmail.com>
2020-01-20 23:01:56 -0800

    CSS update on section header

---
+++ devel/tools/checkin.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+ROOT="$(git rev-parse --show-toplevel)"
+[[ $# == 1 ]] || exit 1
+git commit -a -m "$1"
+git push
+cd "$ROOT"/devel/doc
+aap -f main.aap allhtml
+git checkout gh-pages
+cp *.html ../../doc
+git commit -a -m "$1"
+git push
+git checkout master
