3d7c35dbd64b6d33cb8d7479a6c452e301cd9995
Yian Huang <yianwillis@gmail.com>
2020-01-29 09:57:28 -0800

    add js

---
+++ devel/doc/vimcdoc.js
@@ -0,0 +1,11 @@
+window.addEventListener('DOMContentLoaded', (event) => {
+  const tag = new URLSearchParams(window.location.search).get("tag");
+  if (tag) {
+    for(var i = 0; i < document.links.length; ++i) {
+      const link = document.links[i];
+      if (link.innerHTML == tag) {
+        window.top.location.replace(link.href);
+      }
+    } 
+  }
+});
