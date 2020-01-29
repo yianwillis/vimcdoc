812aaf5e54da447ea9c7ce4e4ffc4747abb0861c
Yian Huang <yianwillis@gmail.com>
2020-01-29 09:58:07 -0800

    add js

--- devel/tools/checkin.sh
+++ devel/tools/checkin.sh
@@ -7,7 +7,7 @@ git push
 cd "$ROOT"/devel/doc
 aap -f main.aap allhtml
 git checkout gh-pages
-cp *.html *.css ../../doc
+cp *.html *.css *.js ../../doc
 git commit -a -m "$1"
 git push
 git checkout master
