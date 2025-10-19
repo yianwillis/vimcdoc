1e9fec4d3de1192006f8613b7eda767dd49f5e3e
Yian Huang <yianwillis@gmail.com>
2025-10-19 00:04:15 -0700

    fix new-style vim script code snippet - attempt 3

--- devel/tools/vim2html.pl
+++ devel/tools/vim2html.pl
@@ -270,12 +270,12 @@ EOF
 
 		# examples
 		elsif( /^>$/ || /\s>$/ || /^>vim$/ || /\>svim$/ || /^>vim9$/ || /\s>vim9$/ ) {
+			$_ = $`;
 			if ( $inexample && /^(<)/ ) {
 				$_ = $';
 				$_ = " " . $_ if /^\s/;
 			}
 			$inexample = 1;
-			chop;
 		}
 		elsif ( $inexample && /^([<\S])/ ) {
 			$inexample = 0;
